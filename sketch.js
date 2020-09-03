a43e0fc90d67a8ba312739ecc9acbd30349b3dbc
JananiVcode <68911390+JananiVcode@users.noreply.github.com>
2020-09-03 21:58:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+
+var bullet;
+var wall;
+var speed;
+var weight;
+var thickness;
+
+function setup() {
+ createCanvas(1600,400);
+ 
+ bullet = createSprite(100,200,50,10);
+ bullet.shapeColor = color(255,255,255);
+ wall= createSprite(1200,203,thickness,height/2);
+ wall.shapeColor= color(80,80,80);
+ speed = random(223,321);
+ weight= random(30,52);
+ thickness = random(22,83);
+ bullet.debug = true;
+ wall.debug= true;
+
+}
+
+function draw() {
+  background(0,0,0);  
+
+  bullet.velocityX = speed;
+
+  hasCollided( 1bullet, 1wall);
+  drawSprites();
+  
+}
+
+function hasCollided( lbullet, lwall){
+
+  bulletRightEdge = lbullet.x + lbullet.width;
+  wallLeftEdge = lwall.x;
+
+  if(hasCollided(wall,bullet)){
+
+    if(bulletRightEdge>= wallLeftEdge){
+      return true;
+    }
+    return false;
+  }
+      bullet.velocityX = 0;
+    
+      var damage = 0.5* weight* speed* speed/(thickness*thickness*thickness);
+    
+    
+      if(damage>10){
+        wall.shapeColor = color(255,0,0);
+      }
+      
+      if(damage<10){
+        wall.shapecolor = color(0,255,0);
+      }
+    }
+
+
+ 
+
+  
+
+
+
+
+
+  
